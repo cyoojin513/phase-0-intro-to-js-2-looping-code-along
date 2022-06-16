986429a92fb3df57011fba86d4e5cfacad09a222
yoojin <yoojin9005@gmail.com>
2022-06-16 00:08:31 -0400

    commit tree 36cc7b78c7d5f778ffde6946abb34224b6ad57f8

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+function writeCards(name, gift) {
+    let messages = [];
+    for (let i = 0; i < name.length; i++) {
+        messages.push(`Thank you, ${name[i]}, for the wonderful ${gift} gift!`)
+    }
+
+    return messages;
+}
+
+
+function countDown() {
+    let count = 10;
+    while (count >= 0) {
+        console.log(count--)
+    debugger;
+    }
+
+  return countDown
+}
